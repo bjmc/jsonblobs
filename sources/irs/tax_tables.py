467cded026a547391c85cfba7f721292a8aed94b
Brendan McCollam <bmccollam@uchicago.edu>
2020-07-19 16:00:11 +0100

    tweak parms

--- sources/irs/tax_tables.py
+++ sources/irs/tax_tables.py
@@ -1,26 +1,38 @@
 from pdfminer.high_level import extract_text
-
-def parse(rawtext):
-	split_blanklines = raw.split('\n\n')
-	chunks = ((12, 13, 14, 15, 16, 17),
-		(18, 19, , 35)
-	)
-	points = []
-	for s, e in chunks:
-		result = process_chunk(split_blanklines[s:e])
-		points.extend(result)
-
+from pdfminer.layout import LAParams
+from itertools import chain
 from collections import namedtuple
-TaxRow = namedtuple('TaxRow', ('lower', 'upper', 'single', 'married_joint', 'married_separate', 'head_of_household'))
 
+TaxRow = namedtuple(
+    'TaxRow',
+    ('lower', 'upper', 'single', 'married_joint', 'married_separate', 'head_of_household'),
+)
 
 def process_chunk(chunks):
-	splitrows = map(lambda x: x.split(), chunks)
-	toint = map(lambda x: int(x.replace(',', '')), splitrows)
-	return [TaxRow(*i) for i in zip(*toint)]
+    splitrows = map(lambda x: x.split(), chunks)
+    toint = map(lambda x: int(x.replace(',', '')), splitrows)
+    return [TaxRow(*i) for i in zip(*toint)]
 
 
-def main(filename)
-	raw = extract_text(filename, page_numbers=range(2,14))
-	chunks = raw.split('\n\n')
-	chunks.index('But\nless\nthan')
+def merge(segments):
+    splitrows = map(lambda x: x.split(), segments)
+    asint = map(lambda x: int(x.replace(',', '')), chain(*splitrows))
+    return list(asint)
+
+def main(filename='2019/i1040tt.pdf'):
+    params = LAParams(
+        line_overlap=0.5,
+        char_margin=1.0,
+        line_margin=0.3,
+        word_margin=0.2,
+        boxes_flow=0.5,
+        detect_vertical=False,
+        all_texts=False,
+    )
+    raw = extract_text(filename, page_numbers=range(2, 14), caching=True, laparams=params)
+    chunks = raw.split('\n\n')
+    col = merge(chunks[29:37])
+    return chunks
+
+if __name__ == '__main__':
+    chunks = main()
