f7a0318c1b213279d405b4195e4e939e96ed37d6
Brendan McCollam <bmccollam@uchicago.edu>
2020-07-19 22:45:39 +0100

    Gonna cleanup

--- sources/irs/table_read.py
+++ sources/irs/table_read.py
@@ -8,32 +8,55 @@ COLUMNS = ('lower', 'upper', 'single', 'married_joint', 'married_separate', 'hea
 
 TaxRow = namedtuple('TaxRow', COLUMNS)
 
-def process_pageN(table):
-	rows = (slice(1, 21), slice(22, 42), slice(43, 63))
-	columns = (slice(0, 6), slice(6, 12), slice(12, 18))
-	segments = []
-	for c in columns:
-		for r in rows:
-			segments.append(table.df.iloc[r, c])
-	for t in segments:
-		t.columns = COLUMNS
-	merged = pd.concat(segments, axis=0)
-	return merged.apply(lambda x: x.str.replace(',', '')).astype(int)
+def clean_and_concat(segments):
+    pass
 
-def process_page1(table):
-	t1 = table.df.iloc[0:62, 0:6]
-	t2 = table.df.iloc[5:62, 6:12]
-	t3 = table.df.iloc[5:62, 12:18]
-	for t in (t1, t2, t3):
-		t.columns = COLUMNS
-	merged = pd.concat((t1, t2, t3), axis=0)
-	merged.replace('', np.nan, inplace=True)
-	merged.dropna(inplace=True)
-	return merged.apply(lambda x: x.str.replace(',', '')).astype(int)
+def process_pageN(df):
+    rows = (slice(1, 21), slice(22, 42), slice(43, 63))
+    columns = (slice(0, 6), slice(6, 12), slice(12, 18))
+    segments = []
+    for c in columns:
+        for r in rows:
+            segments.append(df.iloc[r, c])
+    for t in segments:
+        t.columns = COLUMNS
+    merged = pd.concat(segments, axis=0)
+    return merged.apply(lambda x: x.str.replace(',', '')).astype(int)
+
+def process_page1(df):
+    t1 = df.iloc[0:62, 0:6]
+    t2 = df.iloc[5:62, 6:12]
+    t3 = df.iloc[5:62, 12:18]
+    for t in (t1, t2, t3):
+        t.columns = COLUMNS
+    merged = pd.concat((t1, t2, t3), axis=0)
+    merged.replace('', np.nan, inplace=True)
+    merged.dropna(inplace=True)
+    return merged.apply(lambda x: x.str.replace(',', '')).astype(int)
+
+def process_page14(df):
+    subsets = (
+        (slice(5, 25), slice(0, 6)),
+        (slice(5, 25), slice(6, 12)),
+        (slice(5, 25), slice(12, 18)),
+        (slice(27, 47), slice(0, 6)),
+        (slice(27, 47), slice(6, 12)),
+        (slice(48, 68), slice(0, 6)),
+        (slice(48, 68), slice(6, 12)),
+    )
+    segments = []
+    for rows, columns in subsets:
+        segments.append(df.iloc[rows, columns])
+    for t in segments:
+        t.columns = COLUMNS
+    merged = pd.concat(segments, axis=0)
+    return merged.apply(lambda x: x.str.replace(',', '')).astype(int)
 
 
 def get_tables(filename='i1040tt.pdf'):
-	tables = camelot.read_pdf(filename, flavor='stream', pages='3-14')
-	page1 = process_page1(tables[1])
-
-
+    tables = camelot.read_pdf(filename, flavor='stream', pages='3-14')
+    page1 = process_page1(tables[1])
+    normals = [process_pageN(t) for t in tables[2:11]]
+    cols = sorted(set(range(0, 21)).difference({5, 12, 19}))
+    page13 = process_pageN(tables[11].df.iloc[4:, cols])
+    page14 = process_page14(tables[12].df)
