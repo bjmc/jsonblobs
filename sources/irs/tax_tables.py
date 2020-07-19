76994bad8165e4dec6c88ca8fa791831a7ea6190
Brendan McCollam <bmccollam@uchicago.edu>
2020-07-19 16:44:32 +0100

    Maybe

--- sources/irs/tax_tables.py
+++ sources/irs/tax_tables.py
@@ -3,10 +3,13 @@ from pdfminer.layout import LAParams
 from itertools import chain
 from collections import namedtuple
 
-TaxRow = namedtuple(
-    'TaxRow',
-    ('lower', 'upper', 'single', 'married_joint', 'married_separate', 'head_of_household'),
-)
+COLUMNS = ('lower', 'upper', 'single', 'married_joint', 'married_separate', 'head_of_household')
+START_LINE = 'But\nless\nthan'
+
+TaxRow = namedtuple('TaxRow', COLUMNS)
+
+def parse(seg):
+    return [int(i.replace(',', '')) for i in seg.split()]
 
 def process_chunk(chunks):
     splitrows = map(lambda x: x.split(), chunks)
@@ -19,20 +22,42 @@ def merge(segments):
     asint = map(lambda x: int(x.replace(',', '')), chain(*splitrows))
     return list(asint)
 
+def process(chunks):
+    col = -1
+    cols = list(list() for i in range(6))
+    for lines in chunks:
+        if lines == START_LINE:
+            print("starting")
+            col = 0
+        elif col >= 0:
+            try:
+                segment = parse(lines)
+                print("segment is", segment)
+            except ValueError:
+                sofar = [TaxRow(*i) for i in zip(*cols)]
+                print(sofar)
+                yield sofar
+                col = -1
+                cols = list(list() for i in range(6))
+            else:
+                if segment[0] > cols[col][-1]:
+                    cols[col].extend(segment)
+                else:
+                    col += 1
+
 def main(filename='2019/i1040tt.pdf'):
     params = LAParams(
         line_overlap=0.5,
         char_margin=1.0,
         line_margin=0.3,
         word_margin=0.2,
-        boxes_flow=0.5,
+        boxes_flow=-0.5,
         detect_vertical=False,
         all_texts=False,
     )
     raw = extract_text(filename, page_numbers=range(2, 14), caching=True, laparams=params)
     chunks = raw.split('\n\n')
-    col = merge(chunks[29:37])
-    return chunks
+    return list(process(chunks))
 
 if __name__ == '__main__':
     chunks = main()
