08c9f70d92a266103e9d90419be867dfc72f1d67
Brendan McCollam <bmccollam@uchicago.edu>
2020-07-23 22:53:32 +0100

    Remove non-working extract

--- _sources/irs/tax_tables.py
+++ _sources/irs/tax_tables.py
@@ -1,70 +1,61 @@
-from pdfminer.high_level import extract_text
-from pdfminer.layout import LAParams
-from itertools import chain
-from collections import namedtuple
+import camelot
+
+import pandas as pd
+import numpy as np
 
 COLUMNS = ('lower', 'upper', 'single', 'married_joint', 'married_separate', 'head_of_household')
-START_LINE = 'But\nless\nthan'
 
-TaxRow = namedtuple('TaxRow', COLUMNS)
+def clean_and_concat(segments):
+    for t in segments:
+        t.columns = COLUMNS
+    merged = pd.concat(segments, axis=0)
+    merged.replace('', np.nan, inplace=True)
+    merged.dropna(inplace=True)
+    return merged.apply(lambda x: x.str.replace(',', '')).astype(int)
 
-def parse(seg):
-    return [int(i.replace(',', '')) for i in seg.split()]
+def process_pageN(df):
+    rows = (slice(1, 21), slice(22, 42), slice(43, 63))
+    columns = (slice(0, 6), slice(6, 12), slice(12, 18))
+    segments = []
+    for c in columns:
+        for r in rows:
+            segments.append(df.iloc[r, c])
+    return clean_and_concat(segments)
 
-def process_chunk(chunks):
-    splitrows = map(lambda x: x.split(), chunks)
-    toint = map(lambda x: int(x.replace(',', '')), splitrows)
-    return [TaxRow(*i) for i in zip(*toint)]
+def process_page1(df):
+    t1 = df.iloc[0:62, 0:6]
+    t2 = df.iloc[5:62, 6:12]
+    t3 = df.iloc[5:62, 12:18]
+    segments = (t1, t2, t3)
+    return clean_and_concat(segments)
 
-
-def merge(segments):
-    splitrows = map(lambda x: x.split(), segments)
-    asint = map(lambda x: int(x.replace(',', '')), chain(*splitrows))
-    return list(asint)
-
-def process(chunks):
-    col = -1
-    cols = list(list() for i in range(6))
-    for lines in chunks:
-        print(repr(lines))
-        #print(f"col={col}")
-        #print(f"cols={cols}")
-        if lines == START_LINE:
-            #from pdb import set_trace; set_trace()
-            col = 0
-        elif col >= 0:
-            try:
-                segment = parse(lines)
-                # Skip the big "header" text items
-                if len(segment) == 1:
-                    continue
-            except ValueError:
-                sofar = [TaxRow(*i) for i in zip(*cols)]
-                print(sofar)
-                yield sofar
-                col = -1
-                cols = list(list() for i in range(6))
-            else:
-                if cols[col] and segment[0] < cols[col][-1]:
-                    col += 1
-                cols[col].extend(segment)
-
-def main(filename='2019/i1040tt.pdf'):
-    params = LAParams(
-        line_overlap=0.7,
-        char_margin=1.0,
-        line_margin=0.1,
-        word_margin=0.3,
-        boxes_flow=None,
-        #boxes_flow=0.2,
-        detect_vertical=False,
-        all_texts=False,
+def process_page14(df):
+	top, mid, bottom = slice(5, 25), slice(27, 47), slice(48, 68)
+	left, center, right = slice(0, 6), slice(6, 12), slice(12. 18)
+    subsets = (
+        (top, left),  # 93,000
+        (mid, left),  # 94,000
+        (bottom, left),  # 95,000
+        (top, center),  # 96,000
+        (mid, center),  # 97,000
+        (bottom, center),  # 98,000
+        (top, right),  # 99,000
     )
-    raw = extract_text(filename, page_numbers=range(2, 14), caching=True, laparams=params)
-    chunks = raw.split('\n\n')
-    return chunks
+    segments = []
+    for rows, columns in subsets:
+        segments.append(df.iloc[rows, columns])
+    return clean_and_concat(segments)
 
-if __name__ == '__main__':
-    from pprint import pprint
-    chunks = main()
-    pprint(chunks)
+def get_tables(filename='i1040tt.pdf'):
+    tables = camelot.read_pdf(filename, flavor='stream', pages='3-14')
+    return tables
+
+def reformat(tables):
+    page1 = process_page1(tables[1].df)
+    normals = [process_pageN(t.df) for t in tables[2:11]]
+    cols = sorted(set(range(0, 21)).difference({5, 12, 19}))
+    page13 = process_pageN(tables[11].df.iloc[4:, cols])
+    page14 = process_page14(tables[12].df)
+    total = [page1] + normals + [page13, page14]
+    final = pd.concat(total, axis=0)
+    return final.reset_index(drop=True)
